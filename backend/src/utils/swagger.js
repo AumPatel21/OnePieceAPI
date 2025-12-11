8cfa762ad0f7edcf41cacf4ae03fde870fc08f0d
AumPatel21 <aumpatel2107@gmail.com>
2025-12-10 23:09:54 -0500

    WIP: integrating swagger, adding security scheme but still needs testing

--- backend/src/utils/swagger.js
+++ backend/src/utils/swagger.js
@@ -2,9 +2,30 @@ import swaggerJSDoc from "swagger-jsdoc";
 import swaggerUI from "swagger-ui-express";
 
 const options = {
-    definition: {},
+    definition: {
+        openapi: '3.1.0',
+        info: {
+            title: 'One Piece API',
+            version: '1.0.0'
+        },
+        components: {
+            securitySchemes: {
+                ApiKeyAuth: {
+                    type: 'apiKey',
+                    in: 'header',
+                    name: 'X-API-KEY'
+                }
+            }
+        },
+        security: [
+            { ApiKeyAuth: [] }
+        ]
+    },
     apis: ["./src/docs/openapi.yaml"],
 }
+
+// log the merged definition for debugging if swagger-jsdoc still errors
+console.log('swagger-jsdoc definition:', JSON.stringify(options.definition, null, 2));
 
 const swaggerSpec = swaggerJSDoc(options);
 
