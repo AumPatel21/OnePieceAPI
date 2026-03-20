cb49b402e71100adc7739d306c26b1786cb2fd7d
AumPatel21 <aumpatel2107@gmail.com>
2026-03-19 23:06:43 -0400

    added esling config with node globals

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,8 +1,26 @@
+// import js from "@eslint/js";
+
+// export default [
+//     js.configs.recommended,
+//     {
+//         rules: {
+//             "no-unused-vars": "warn",
+//             "no-undef": "warn"
+//         }
+//     }
+// ];
+
 import js from "@eslint/js";
+import globals from "globals"; //
 
 export default [
     js.configs.recommended,
     {
+        languageOptions: {
+            globals: {
+                ...globals.node // Adds process, console, etc.
+            }
+        },
         rules: {
             "no-unused-vars": "warn",
             "no-undef": "warn"
